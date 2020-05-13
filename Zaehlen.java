7b4668a20ae29f3e63867842349cd9107c9bc419
Arne Brück <brueck@ohgspringe.de>
2020-05-13 12:12:09 +0200

    Method countdown complete

---
+++ Zaehlen.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class Zaehlen here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Zaehlen
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    public Zaehlen( int max )
+    {
+        x = max;
+    }
+    
+    public static void main(String[] args)
+    {
+       Zaehlen myObject = new Zaehlen(10);
+       myObject.countdown(); 
+        
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void countdown()
+    {
+        while(x >= 0){
+            System.out.println(x--);
+        }
+    }
+}
